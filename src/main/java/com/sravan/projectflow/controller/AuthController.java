65f9455fb417f1c14e90a5c3c1a1cf11a35f8404
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-18 21:59:00 +0530

    Created authentication REST APIs for user registration and login

---
+++ src/main/java/com/sravan/projectflow/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.sravan.ProjectFlow.controller;
+
+import com.sravan.ProjectFlow.dto.AuthResponse;
+import com.sravan.ProjectFlow.dto.LoginRequest;
+import com.sravan.ProjectFlow.dto.RegisterRequest;
+import com.sravan.ProjectFlow.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+
+        return authService.login(request);
+    }
+}
+
