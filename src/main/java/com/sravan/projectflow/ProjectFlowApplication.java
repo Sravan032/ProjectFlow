ff1a11eff3b9e20f35807f4840090a42a727f164
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-18 21:53:58 +0530

    Initial Spring Boot project setup with MySQL configuration

---
+++ src/main/java/com/sravan/projectflow/ProjectFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sravan.ProjectFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFlowApplication.class, args);
+	}
+
+}
