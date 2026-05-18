67f212380af9deea6cdd7c07ca139af43dca2030
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-18 21:58:03 +0530

    Configured Spring Security for allowing API requests and implement JWT utility

---
+++ src/main/java/com/sravan/projectflow/security/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.sravan.ProjectFlow.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY =
+            "mysecretkeymysecretkeymysecretkey";
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                )
+                .signWith(
+                        SignatureAlgorithm.HS256,
+                        SECRET_KEY
+                )
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token, String email) {
+
+        String extractedEmail = extractEmail(token);
+
+        return extractedEmail.equals(email);
+    }
+}
