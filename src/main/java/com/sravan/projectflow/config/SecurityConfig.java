2632b6607c8be1642ffff0fb01acab9e8df77acd
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-05-21 17:05:24 +0530

    Updated security config and jwtUtil

--- src/main/java/com/sravan/projectflow/config/SecurityConfig.java
+++ src/main/java/com/sravan/projectflow/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig {
@@ -13,11 +14,39 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource
+    corsConfigurationSource() {
+
+        CorsConfiguration config =
+                new org.springframework.web.cors.CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:5173");
+
+        config.addAllowedMethod("*");
+
+        config.addAllowedHeader("*");
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource
+                source =
+                new org.springframework.web.cors.
+                        UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-            http.csrf(csrf -> csrf.disable())
-                    .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+            http
+                    .cors(cors -> {})
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**",
+                                    // SWAGGER
+                                    "/v3/api-docs/**",
+                                    "/swagger-ui/**",
+                                    "/swagger-ui.html").permitAll()
                             .anyRequest().authenticated()
                     );
             return http.build();
--- src/main/java/com/sravan/projectflow/security/JwtUtil.java
+++ src/main/java/com/sravan/projectflow/security/JwtUtil.java
@@ -4,10 +4,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     private static final String SECRET =
             "mysecretkeymysecretkeymysecretkey123456";
